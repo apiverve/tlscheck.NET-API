57e8e70bbfe89024f22ec8aad82328089b349ef4
APIVerve Team <hello@apiverve.com>
2025-04-23 11:25:10 -0500

    Bug Fixes

---
+++ TLSCheckerQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class TLSCheckerQueryOptions {
+public string domain { get; set; }
+public number port { get; set; }
+}
+
+}
